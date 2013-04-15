7fb2f9f4d357b3e16f5b46ba71a8a0744e34981f
Christophe Desguez <christophe.desguez@gmail.com>
2013-04-15 20:49:16 +0200

    Now just 1081bytes (minified)

--- js/bip.js
+++ js/bip.js
@@ -13,15 +13,15 @@
  * http://github.com/zipang/bip
  * MIT Licence
  */
-(function(win, doc) {
+(function (win, doc) {
 
-    var html = doc.getElementsByTagName("html")[0],
-        head = doc.getElementsByTagName("head")[0],
-        screenWidth = win.screen.width;
+	var html = doc.getElementsByTagName("html")[0],
+		head = doc.getElementsByTagName("head")[0],
+		screenWidth = win.screen.width;
 
-    /* ==================
-       Utility Belt
-       ================== */
+	/* ==================
+	 Utility Belt
+	 ================== */
 
 	function onDOMReady(callback) { // Taken (more or less) from jQuery
 
@@ -35,6 +35,7 @@
 					callback.called = true;
 				}
 			}
+
 			// Use the handy event callback
 			doc.addEventListener("DOMContentLoaded", cb_once, false);
 
@@ -50,72 +51,70 @@
 	 * @param pathLoader {Function} image path resolver (takes an image key and the current breakpoint to return the image path)
 	 * @return [] the array of images to load
 	 */
-    function assignBg(bgDef, pathLoader) {
+	function assignBg(bgDef, pathLoader) {
 
-        if (bgDef.length) return bgDef; // only an array of images > no assignation
+		if (bgDef.length) return bgDef; // only an array of images > no assignation
 
-        var output = [], images = [], i = 0;
-        for (var key in bgDef) {
-          output.push(key, " {background-image: url(", pathLoader(images[i++] = bgDef[key]), ");}\n");
-        }
-        var styleElt = doc.createElement("STYLE"),
-            rules = doc.createTextNode(output.join(""));
+		var output = [], images = [], i = 0;
+		for (var key in bgDef) {
+			output.push(key, " {background-image: url(", pathLoader(images[i++] = bgDef[key]), ");}\n");
+		}
+		var styleElt = doc.createElement("STYLE"),
+			rules = doc.createTextNode(output.join(""));
 
-        styleElt.appendChild(rules);
-        head.appendChild(styleElt);
-        return images;
-    }
+		styleElt.appendChild(rules);
+		head.appendChild(styleElt);
+		return images;
+	}
 
 	function getImage(image) {
 		return image;
 	}
 
-    win["bip"] = {
-        preload: function(p_images, p_options) {
+	win["bip"] = {
+		preload:function (p_images, p_options) {
 
-            if (doc.images) {
-                var options = p_options || {},
-                    className = (options.className !== undefined ? options.className : "bg-preload"),
-                    breakpoints = options.breakpoints || false,
-                    breakpoint = screenWidth,
-                    callback = options.callback || (win.console ? console.dir : getImage);
+			if (doc.images) {
+				var options = p_options || {},
+					className = (options.className !== undefined ? options.className : "bg-preload"),
+					breakpoint, breakpoints = options.breakpoints || [screenWidth],
+					callback = options.callback || (win.console ? console.dir : getImage);
 
-                // Find the breakpoint matching our screen resolution
-                // example : breakpoints = [420, 1024, 1600], screenWidth = 800 >> breakpoint = 420
-                if (breakpoints) while ((breakpoint = breakpoints.pop() || "mobile") > screenWidth) {}
+				// Find the breakpoint matching our screen resolution
+				// example : breakpoints = [420, 1024, 1600], screenWidth = 800 >> breakpoint = 420
+				while ((breakpoint = breakpoints.pop() || "mobile") > screenWidth) {};
 
-                function loader(img) {
-                    return (options.pathLoader || getImage)(img, breakpoint);
-                }
+				function load(img) {
+					return (options.pathLoader || getImage)(img, breakpoint);
+				}
 
-                var images = assignBg(p_images, loader),
-                    count = images.length, i = 0, result = {};
+				var images = assignBg(p_images, load),
+					count = images.length, result = {};
 
-                if (count && className) html.className += (" " + className);
+				if (count && className) html.className += (" " + className);
 
-                function loaded () {
-                    if (className) html.className = html.className.replace(className, "");
-                    callback(result);
-                }
+				function loaded() {
+					if (className) html.className = html.className.replace(className, "");
+					callback(result);
+				}
 
-                function onloadHandler(success) {
-                    return function() {
-                        result[this.src] = success;
-                        if (!(--count)) onDOMReady(loaded);
-                    }
-                }
+				function onloadHandler(success) {
+					return function () {
+						result[this.src] = success;
+						if (!(--count)) onDOMReady(loaded);
+					}
+				}
 
-                function watch(img) {
-                    img.onload  = onloadHandler(true);
-                    img.onerror = onloadHandler(false);
-                    return img;
-                }
+				function watch(img) {
+					img.onload = onloadHandler(true);
+					img.onerror = onloadHandler(false);
+					return img;
+				}
 
-                while (i < count) {
-                    watch(new Image()).src = loader(images[i++], breakpoint);
-                }
-            }
-        }
-    }; // bip
+				while (images.length) watch(new Image()).src = load(images.pop());
+
+			}
+		}
+	}; // bip
 
 })(window, document);
